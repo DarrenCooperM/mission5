bcbe88eee7d347929f82f0bb1d1369e6ce592c49
Darren Cooper-Matila <dcoopermatila@gmail.com>
2023-01-15 12:09:10 +1300

    completed static frontend of contact page. Need to connect the content stuff with the backend

--- frontend/src/components/contact/ContactContent.js
+++ frontend/src/components/contact/ContactContent.js
@@ -1,7 +1,18 @@
 import React from "react";
 import styles from "../styles/contact/ContactContent.module.css";
+import { useState, useEffect } from "react";
 
 export default function ContactContent() {
+  const [imageURL, setImageURL] = useState("");
+  // getting the image from db
+  useEffect(() => {
+    fetch("http://localhost:7070/contentsImg/content-images")
+      .then((res) => res.json())
+      .then((imageData) => {
+        console.log(imageData);
+        setImageURL(imageData);
+      });
+  }, []);
   return (
     <div className={styles.contentContainer}>
       <div className={styles.contentFeatures}>
@@ -16,37 +27,49 @@ export default function ContactContent() {
             <button className={styles.leftContentBtn}>Back</button>
           </a>
         </div>
-        <div className={styles.rightContent}>
-          <input
-            className={styles.rightContentInputBox}
-            type="text"
-            name="name"
-            autoComplete="off"
-            placeholder="Your Name"
-          />
-          <input
-            className={styles.rightContentInputBox}
-            type="text"
-            name="email"
-            autoComplete="off"
-            placeholder="Your Email"
-          />{" "}
-          <br />
-          <input
-            className={styles.rightContentInputBox}
-            type="text"
-            name="number"
-            autoComplete="off"
-            placeholder="Phone Number"
-          />{" "}
-          <br />
-          <input
-            className={styles.rightContentMsgBox}
-            type="text"
-            name="message"
-          />
+        <div className={styles.rightContentContainer}>
+          <div className={styles.rightContentInputBoxContainer}>
+            <input
+              className={styles.rightContentInputBox}
+              type="text"
+              name="name"
+              autoComplete="off"
+              placeholder="Your Name"
+            />
+            <input
+              className={styles.rightContentInputBox}
+              type="text"
+              name="email"
+              autoComplete="off"
+              placeholder="Your Email"
+            />
+            <input
+              className={styles.rightContentInputBox}
+              type="text"
+              name="number"
+              autoComplete="off"
+              placeholder="Phone Number"
+            />
+          </div>
+          <div className={styles.rightContentMsgContainer}>
+            <h4 className={styles.rightContentMsgHeader}>Message</h4>
+            <input
+              className={styles.rightContentMsgBox}
+              type="text"
+              name="message"
+              autoComplete="off"
+            />
+            <div className={styles.sendBtnContainer}>
+              <button className={styles.sendBtn}>Send</button>
+            </div>
+          </div>
         </div>
       </div>
+      <img
+        className={styles.contentImage}
+        src={imageURL && imageURL[3].url}
+        alt="aptImg"
+      />
     </div>
   );
 }
